6160674c537f8dcc4ca14e4d521295518e24fa08
lmchun <lmchun@sbcglobal.net>
2024-03-17 13:22:04 -0400

    initial set up

---
+++ src/analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+## import dot environment library. put key in .env file. import that into the python. Can also look into setting it up with ur direnv setup
+
+
+
+# df = pd.read_csv('myfile.csv')
+# this is where to manage the file and then do the analysis here
